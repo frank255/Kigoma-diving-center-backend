092bb0e2484374f5365988ddb627b5ccc961471a
Frank Ilunga <ilungafrank3@gmail.com>
2023-08-06 22:58:26 +0300

    fix: punctuation mark removal

--- app/Notifications/BookingCreated.php
+++ app/Notifications/BookingCreated.php
@@ -46,7 +46,7 @@ class BookingCreated extends Notification
             ->subject('Booking Confirmation')
             ->greeting("Dear, {$this->booking->fullname}")
             ->line("We are delighted that Your booking is well received.Kindly allow us to work on it and our team will shortly contact you back.")
-            ->line('Thank you for putting trusting on us!, wishing you unforgettable experience.');
+            ->line('Thank you for putting trusting on us, wishing you unforgettable experience.');
     }
 
     /**
